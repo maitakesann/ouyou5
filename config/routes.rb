f1e76b36bd27c03cd480706f08ce7f04d41ccd11
maitakesann <kankore1204a@yahoo.co.jp>
2021-05-08 06:16:05 +0000

    ouyou5

---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root 'homes#top'
+  get 'home/about' => 'homes#about'
+
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get :follows
+    get :followers
+  end
+  resources :books do
+  resource :favorites, only: [:create, :destroy]
+  resources :book_comments, only: [:create, :destroy]
+  end
+end
